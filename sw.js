037cf1eae26ebd190c4da6baf425a850d68da7e5
Alex Durán <37418934+AlexDuOl@users.noreply.github.com>
2026-02-06 01:29:35 -0600

    Update sw.js
    Ajustes

--- sw.js
+++ sw.js
@@ -1,40 +1,18 @@
-const CACHE_NAME = 'cotizador-3d-v2';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.tailwindcss.com'
-];
-
-// Instalación: Guarda los archivos en la caché
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-});
-
-// Activación: Limpia versiones viejas de la caché
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Estrategia de carga: Intenta internet, si falla usa la caché
+// Estrategia: Network First (Prioriza internet para tener lo último del GitHub)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        // Si hay internet, guarda la nueva versión en caché
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        // Si NO hay internet, usa lo que tenemos guardado
+        return caches.match(event.request);
+      })
   );
 });
+
